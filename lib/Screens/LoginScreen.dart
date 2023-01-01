7de0da6fca4e667bfd7490db2f79246d856a9f1f
Arjun Ingole <arjuningole38@gmail.com>
2023-01-02 01:33:40 +0530

    loginscreen: Do some UI improvements
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:osiris/Services/auth.dart';
+import 'package:osiris/Services/consts.dart';
 import 'package:provider/provider.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -9,47 +10,61 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return SafeArea(
-        child: Container(
-      color: Color(0xFF111015),
-      height: size.height,
-      width: size.width,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image(
-            width: size.width * 0.60,
-            image: const AssetImage(
-              "assets/AuthDuck.gif",
-            ),
-          ),
-          Center(
+    return Container(
+        color: Color(0xFF111015),
+        child: SafeArea(
             child: Container(
-                margin: EdgeInsets.all(24),
-                width: double.infinity,
-                height: 72,
-                child: ElevatedButton.icon(
-                  onPressed: () {
-                    final provider = Provider.of<GoogleSignInProvider>(context,
-                        listen: false);
-                    provider.googleLogin();
-                  },
-                  icon: const FaIcon(
-                    FontAwesomeIcons.google,
-                    color: Colors.white,
-                  ),
-                  label: const Text(
-                    "Sign In with Google",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(14)),
-                      backgroundColor: Color(0xFF2A292F)),
-                )),
-          )
-        ],
-      ),
-    ));
+          color: Color(0xFF111015),
+          height: size.height,
+          width: size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: size.height * 0.3,
+              ),
+              Image(
+                width: size.width * 0.60,
+                image: const AssetImage(
+                  "assets/AuthDuck.gif",
+                ),
+              ),
+              Center(
+                child: Container(
+                    margin: EdgeInsets.all(24),
+                    width: double.infinity,
+                    height: 72,
+                    child: ElevatedButton.icon(
+                      onPressed: () {
+                        final provider = Provider.of<GoogleSignInProvider>(
+                            context,
+                            listen: false);
+                        provider.googleLogin();
+                      },
+                      icon: const FaIcon(
+                        FontAwesomeIcons.google,
+                        color: Colors.white,
+                      ),
+                      label: const Text(
+                        "Sign In with Google",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(14)),
+                          backgroundColor: Color(0xFF2A292F)),
+                    )),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(28, 0, 28, 24),
+                child: Text(
+                  footer_text,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Color(0xFF423E50)),
+                ),
+              )
+            ],
+          ),
+        )));
   }
 }
--- lib/Services/consts.dart
+++ lib/Services/consts.dart
@@ -1,1 +1,2 @@
-
+String footer_text =
+    "Sign in to access and save your personal movie and show collection on Osiris. Start your journey through the world of film and television today!";
