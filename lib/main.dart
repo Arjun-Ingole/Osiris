44874f07b4d84b91f09a40473947808b38574772
Arjun Ingole <arjuningole38@gmail.com>
2023-01-01 23:00:00 +0530

    main: add firebase core and inilize firebase before everything
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:osiris/Screens/HomeScreen.dart';
 
-void main() => runApp(MaterialApp(home: App()));
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp;
+  runApp(MaterialApp(home: App()));
+}
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
