9f3c35ff528802c26926fe54e6c45f160273609f
Arjun Ingole <arjuningole38@gmail.com>
2023-01-08 01:07:13 +0530

    widget: get rid of dead code
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

--- lib/Widgets/CarouselCard.dart
+++ lib/Widgets/CarouselCard.dart
@@ -41,34 +41,3 @@ class CustomCarouselSlider extends StatelessWidget {
     );
   }
 }
-
-
-
-// FutureBuilder(
-//       future: future,
-//       builder: ((context, AsyncSnapshot snapshot) {
-//         if (snapshot.hasData) {
-//           return CarouselSlider.builder(
-//             itemCount: 20,
-//             options: CarouselOptions(
-//               autoPlay: true,
-//               autoPlayInterval: const Duration(seconds: 3),
-//               autoPlayAnimationDuration: const Duration(milliseconds: 800),
-//             ),
-//             itemBuilder: ((context, index, realIndex) {
-//               var url = snapshot.data![index].posterPath.toString();
-//               return LandingCard(
-//                   NetworkImage("https://image.tmdb.org/t/p/w500$url"),
-//                   snapshot.data![index].title.toString());
-//             }),
-//           );
-//         } else {
-//           return CarouselSlider.builder(
-//               itemCount: 1,
-//               options: CarouselOptions(),
-//               itemBuilder: ((context, index, realIndex) {
-//                 return LandingCard(
-//                     const AssetImage("assets/LoadingImage.png"), "Loading");
-//               }));
-//         }
-//       }));
