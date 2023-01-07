77177d6f8dc78b9709d5c01274e0004eb0b1ac04
Arjun Ingole <arjuningole38@gmail.com>
2023-01-07 21:33:11 +0530

    services: write function to get popular shows
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

--- lib/Services/API.dart
+++ lib/Services/API.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:osiris/Models/PopularMovies.dart';
+import 'package:osiris/Models/TvShow.dart';
 import 'package:osiris/Services/key.dart';
 
 class APIService {
@@ -14,6 +15,32 @@ class APIService {
       var movies = response.data['results'] as List;
       List<Results> movieList = movies.map((m) => Results.fromJson(m)).toList();
       return movieList;
+    } catch (error, stacktrace) {
+      throw Exception(
+          'Exception accoured: $error with stacktrace: $stacktrace');
+    }
+  }
+
+  Future<List<Results>> getTopRatedMovie() async {
+    try {
+      final url = '$baseUrl/movie/top_rated?$apiKey&page=1';
+      final response = await _dio.get(url);
+      var movies = response.data['results'] as List;
+      List<Results> movieList = movies.map((m) => Results.fromJson(m)).toList();
+      return movieList;
+    } catch (error, stacktrace) {
+      throw Exception(
+          'Exception accoured: $error with stacktrace: $stacktrace');
+    }
+  }
+
+  Future<List<TvShow>> getPopularShow() async {
+    try {
+      final url = '$baseUrl/tv/popular?$apiKey&page=1';
+      final response = await _dio.get(url);
+      var shows = response.data['results'] as List;
+      List<TvShow> showsList = shows.map((m) => TvShow.fromJson(m)).toList();
+      return showsList;
     } catch (error, stacktrace) {
       throw Exception(
           'Exception accoured: $error with stacktrace: $stacktrace');
