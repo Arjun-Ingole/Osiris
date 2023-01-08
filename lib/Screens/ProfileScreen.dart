5bfc8ac73e784317aaab5bf6e907291339c28a61
Arjun Ingole <arjuningole38@gmail.com>
2023-01-09 00:36:14 +0530

    screen: add a basic profile screen
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

---
+++ lib/Screens/ProfileScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+    );
+  }
+}
