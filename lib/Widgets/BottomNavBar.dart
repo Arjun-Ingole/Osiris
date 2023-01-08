39a9b57313f21413089fc578c762f1a6ef340e5d
Arjun Ingole <arjuningole38@gmail.com>
2023-01-08 17:26:09 +0530

    widget: Create custom bottom nav bar
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

---
+++ lib/Widgets/BottomNavBar.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:unicons/unicons.dart';
+import 'package:osiris/Services/consts.dart';
+
+class BottomNavBar extends StatefulWidget {
+  @override
+  _BottomNavBarState createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      margin: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+          color: accent_t.withOpacity(0.95),
+          borderRadius: BorderRadius.circular(12)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          IconButton(
+            icon: Icon(
+              UniconsLine.home_alt,
+              color: _currentIndex == 0 ? Colors.white : inactive_accent,
+            ),
+            onPressed: () {
+              setState(() {
+                _currentIndex = 0;
+              });
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              UniconsLine.search,
+              color: _currentIndex == 1 ? Colors.white : inactive_accent,
+            ),
+            onPressed: () {
+              setState(() {
+                _currentIndex = 1;
+              });
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              UniconsLine.heart,
+              color: _currentIndex == 2 ? Colors.white : inactive_accent,
+            ),
+            onPressed: () {
+              setState(() {
+                _currentIndex = 2;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
