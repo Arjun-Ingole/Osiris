716de9c6b1f660af4e7615bbc709dc90c11cc294
Arjun Ingole <arjuningole38@gmail.com>
2023-01-08 19:42:16 +0530

    consts: add more colors
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

--- lib/Services/consts.dart
+++ lib/Services/consts.dart
@@ -7,3 +7,5 @@ String sign_in_text = "Sign In with Google";
 // Theme
 Color background_primary = const Color(0xFF111015);
 Color accent_secondary = const Color(0xFF00FFD1);
+Color accent_t = const Color(0xFF2A292F);
+Color inactive_accent = const Color(0xFF494949);
