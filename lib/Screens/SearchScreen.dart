1ebfb1d6a65b19e270d760e74eed8d214c55d117
Arjun Ingole <arjuningole38@gmail.com>
2023-01-09 00:35:05 +0530

    screen: add a basic search screen
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

---
+++ lib/Screens/SearchScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+    );
+  }
+}
