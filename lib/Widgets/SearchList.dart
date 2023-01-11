35d6d2abede8ce11c423824425e81682e1e31350
Arjun Ingole <arjuningole38@gmail.com>
2023-01-11 19:44:44 +0530

    widget: create somewhat functional search result list
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

---
+++ lib/Widgets/SearchList.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:osiris/Models/SearchResult.dart';
+import 'package:osiris/Widgets/MovieCard.dart';
+
+class SearchList extends StatefulWidget {
+  SearchList(this.future, {super.key});
+
+  Future<List<SearchResult>> future;
+
+  @override
+  State<SearchList> createState() => _SearchListState();
+}
+
+class _SearchListState extends State<SearchList> {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return FutureBuilder(
+      future: widget.future,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Container(
+            height: size.height * 0.8,
+            child: ListView.builder(
+              scrollDirection: Axis.vertical,
+              physics: const BouncingScrollPhysics(),
+              itemCount: snapshot.data!.length,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                var url = snapshot.data![index].backdropPath;
+                return MovieCard(
+                  snapshot.data![index].title.toString(),
+                  url == null
+                      ? const AssetImage("assets/LoadingImage.png")
+                      : CachedNetworkImageProvider(
+                              "https://image.tmdb.org/t/p/original$url")
+                          as ImageProvider,
+                );
+              },
+            ),
+          );
+        } else {
+          return MovieCard(
+              "Loading", const AssetImage("assets/LoadingImage.png"));
+        }
+      },
+    );
+  }
+}
