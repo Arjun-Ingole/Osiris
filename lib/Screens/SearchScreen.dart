eed5673414aab5c9069386e713aec78c17c2a7ee
Arjun Ingole <arjuningole38@gmail.com>
2023-01-11 19:47:55 +0530

    screen: create search screen with textField
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

--- lib/Screens/SearchScreen.dart
+++ lib/Screens/SearchScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:osiris/Services/API.dart';
 import 'package:osiris/Services/consts.dart';
 import 'package:osiris/Widgets/BottomNavBar.dart';
+import 'package:osiris/Widgets/SearchList.dart';
 import 'package:unicons/unicons.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -11,10 +13,19 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  final myController = TextEditingController();
+  String query = "";
+
   @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         backgroundColor: background_primary,
         bottomNavigationBar: BottomNavBar(currentIndex: 1),
         extendBody: true,
@@ -26,28 +37,41 @@ class _SearchScreenState extends State<SearchScreen> {
               fit: BoxFit.fitWidth,
               width: size.width,
             ),
-            Container(
-              height: 50,
-              margin: const EdgeInsets.fromLTRB(8, 28, 8, 20),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                color: accent_t.withOpacity(0.95),
-              ),
-              child: TextField(
-                cursorColor: accent_secondary,
-                maxLines: 1,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-                decoration: const InputDecoration(
-                    border: InputBorder.none,
-                    prefixIcon: Icon(
-                      UniconsLine.search,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  height: 50,
+                  margin: const EdgeInsets.fromLTRB(8, 28, 8, 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: accent_t.withOpacity(0.95),
+                  ),
+                  child: TextField(
+                    controller: myController,
+                    cursorColor: accent_secondary,
+                    maxLines: 1,
+                    style: const TextStyle(
                       color: Colors.white,
+                      fontSize: 20,
                     ),
-                    prefixIconColor: Colors.white),
-              ),
+                    decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(
+                          UniconsLine.search,
+                          color: Colors.white,
+                        ),
+                        prefixIconColor: Colors.white),
+                    onChanged: (value) {
+                      setState(() {
+                        query = myController.text;
+                      });
+                    },
+                  ),
+                ),
+                SearchList(APIService().getSearchResult(query))
+              ],
             ),
           ],
         ));
