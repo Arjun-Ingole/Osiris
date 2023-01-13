bfbd0e334043651e765751fa96c0ec3b464b25b1
Arjun Ingole <arjuningole38@gmail.com>
2023-01-13 20:00:44 +0530

    screen: create placeholder for movie screen
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

---
+++ lib/Screens/MovieScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MovieScreen extends StatefulWidget {
+  const MovieScreen({super.key});
+
+  @override
+  State<MovieScreen> createState() => _MovieScreenState();
+}
+
+class _MovieScreenState extends State<MovieScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
