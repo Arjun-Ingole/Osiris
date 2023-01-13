9baa4977b3d36eef97e0ec4c08a0ae1012c49647
Arjun Ingole <arjuningole38@gmail.com>
2023-01-13 20:01:52 +0530

    screen : create placeholder for TVShowScreen
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

---
+++ lib/Screens/TvShowScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TVShowScreen extends StatefulWidget {
+  const TVShowScreen({super.key});
+
+  @override
+  State<TVShowScreen> createState() => _TVShowScreenState();
+}
+
+class _TVShowScreenState extends State<TVShowScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
