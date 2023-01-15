b716821e531fec09436fa8472d62772eb5ef880a
Arjun Ingole <arjuningole38@gmail.com>
2023-01-15 23:23:03 +0530

    screen: main: convert to stateful widget
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

--- lib/Screens/MainScreen.dart
+++ lib/Screens/MainScreen.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
-    );
+    return Container();
   }
 }
