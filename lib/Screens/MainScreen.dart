ac83ce6ecbbb63a38bbd50cbefa4fbeed94dd5ff
Arjun Ingole <arjuningole38@gmail.com>
2023-01-15 23:26:30 +0530

    mainscreen: create basic main screen
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

--- lib/Screens/MainScreen.dart
+++ lib/Screens/MainScreen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:osiris/Services/consts.dart';
+import 'package:osiris/Services/API.dart';
+import 'package:osiris/Widgets/BottomNavBar.dart';
+import 'package:osiris/Widgets/CarouselCard.dart';
+import 'package:osiris/Widgets/CustomLists.dart';
+import 'package:osiris/Widgets/SectionText.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -10,6 +16,33 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      bottomNavigationBar: BottomNavBar(currentIndex: 0),
+      extendBody: true,
+      body: Container(
+        height: size.height,
+        width: size.width,
+        color: background_primary,
+        child: ListView(
+          cacheExtent: 9999,
+          padding: EdgeInsets.zero,
+          scrollDirection: Axis.vertical,
+          physics: const BouncingScrollPhysics(),
+          shrinkWrap: true,
+          children: [
+            CustomCarouselSlider(APIService().getTopRatedShow()),
+            SectionText("Popular", "Movies"),
+            CustomListMovie(APIService().getPopularMovie()),
+            SectionText("TOP Rated", "Movies"),
+            CustomListMovie(APIService().getTopRatedMovie()),
+            SectionText("Popular", "Shows"),
+            CustomListTV(APIService().getPopularShow()),
+            SectionText("NoW PLAying", "Movies"),
+            CustomListMovie(APIService().getNowPLayingMovie()),
+          ],
+        ),
+      ),
+    );
   }
 }
