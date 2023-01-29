682a89c446f603bcffc11f8fbb92684ede1c4481
Arjun Ingole <arjuningole38@gmail.com>
2023-01-30 00:57:33 +0530

    widget: navigate to routes from movie card
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

--- lib/Widgets/MovieCard.dart
+++ lib/Widgets/MovieCard.dart
@@ -1,29 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:go_router/go_router.dart';
 
-Widget MovieCard(String title, ImageProvider image) {
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: [
-      Container(
-        height: 150,
-        width: 100,
-        margin: EdgeInsets.fromLTRB(8, 4, 8, 4),
-        decoration: BoxDecoration(
-          image: DecorationImage(fit: BoxFit.cover, image: image),
-          borderRadius: BorderRadius.circular(10),
-        ),
+class MovieCard extends StatelessWidget {
+  MovieCard(this.title, this.image, this.Id, this.mediaType, {super.key});
+  String title;
+  ImageProvider image;
+  String Id;
+  String mediaType;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        HapticFeedback.mediumImpact();
+        GoRouter.of(context).push('/$mediaType/$Id');
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: 150,
+            width: 100,
+            margin: EdgeInsets.fromLTRB(8, 4, 8, 4),
+            decoration: BoxDecoration(
+              image: DecorationImage(fit: BoxFit.cover, image: image),
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          Container(
+            width: 100,
+            child: Text(
+              title,
+              style: TextStyle(color: Colors.white),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.center,
+            ),
+          )
+        ],
       ),
-      Container(
-        width: 100,
-        child: Text(
-          title,
-          style: TextStyle(color: Colors.white),
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-          textAlign: TextAlign.center,
-        ),
-      )
-    ],
-  );
+    );
+  }
 }
