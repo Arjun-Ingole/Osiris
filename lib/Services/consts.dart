de3f2a7d3928f14026ef6625dab48d404d0911a2
Arjun Ingole <arjuningole38@gmail.com>
2023-02-10 14:54:10 +0530

    consts: create sample color palette
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

--- lib/Services/consts.dart
+++ lib/Services/consts.dart
@@ -9,3 +9,14 @@ Color background_primary = const Color(0xFF111015);
 Color accent_secondary = const Color(0xFF00FFD1);
 Color accent_t = const Color(0xFF34323A);
 Color inactive_accent = const Color(0xFF494949);
+
+List<Color> ColorPallete = [
+  Color(0xFF14303B),
+  Color(0xFF152C22),
+  Color(0xFF545551),
+  Color(0xFF342A30),
+  Color(0xFF3B3B2F),
+  Color(0xFF382E39),
+  Color(0xFF0F1D39),
+  Color(0xFF361E20)
+];
