27fe8351393ef8b8f49395eb7e5724534ed7c778
Arjun Ingole <arjuningole38@gmail.com>
2023-04-09 17:49:39 +0530

    screen: simplify login redirection logic
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

--- lib/Screens/NavScreen.dart
+++ lib/Screens/NavScreen.dart
@@ -3,30 +3,21 @@ import 'package:flutter/material.dart';
 import 'package:osiris/Screens/LoginScreen.dart';
 import 'package:osiris/Screens/MainScreen.dart';
 
-class NavScreen extends StatelessWidget {
+class NavScreen extends StatefulWidget with RouteAware {
   const NavScreen({super.key});
 
+  @override
+  State<NavScreen> createState() => _NavScreenState();
+}
+
+class _NavScreenState extends State<NavScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          if (snapshot.hasError) {
-            return Center(
-              child: Text("Something went wrong"),
-            );
-          }
-          if (snapshot.hasData) {
-            return MainScreen();
-          } else {
-            return LoginScreen();
-          }
+          return snapshot.hasData ? MainScreen() : LoginScreen();
         },
       ),
     );
